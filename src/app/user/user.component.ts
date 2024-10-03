2880e54bf37ad61209a6ef485e464841e9e72f46
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-03 15:54:36 +0200

    created a button

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,CommonModule,MatIconModule,MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
