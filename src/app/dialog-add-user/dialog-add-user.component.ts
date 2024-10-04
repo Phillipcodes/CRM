ecbfdd024ec750d3a85a2405ec3b0b9a08f54e73
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-04 17:35:46 +0200

    polished and adde multiple field to the dialog

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -5,11 +5,12 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [CommonModule,MatDialogModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,],
+  imports: [CommonModule,MatDialogModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,MatButtonModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
