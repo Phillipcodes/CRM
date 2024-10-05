02d48e2fd04a2b55aaecd5ec54b083a006c79b9c
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-05 20:22:14 +0200

    added email field

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -5,23 +5,28 @@ export class User {
     street: string;
     zipCode: number;
     city: string;
+    email:string
 
     constructor(obj?: any) {
         this.firstName = obj ? obj.firstName : "";
         this.lastName = obj ? obj.lastName : "";
+        this.email = obj ? obj.city : "";
         this.birthDate = obj ? obj.birthDate : "";
         this.street = obj ? obj.street : "";
         this.zipCode = obj ? obj.zipCode : "";
         this.city = obj ? obj.city : "";
+        
     }
     toJSON() {
         return {
             firstName: this.firstName,
             lastName: this.lastName,
+            email: this.email,
             birthDate: this.birthDate,
             street: this.street,
             zipCode: this.zipCode,
             city: this.city,
+          
         }
     }
 }
