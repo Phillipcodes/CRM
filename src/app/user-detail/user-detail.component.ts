6e517b0b5889288856d147ebdfc1825ec7b22fd0
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-06 13:50:55 +0200

    start of design for user details

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CommonModule,MatCard,MatCardContent],
+  imports: [CommonModule,MatCard,MatCardContent,MatCardHeader,MatCardTitle],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
