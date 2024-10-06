d9e34a6a1af868709beccbdc760ae2bb5cd912c8
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-06 13:44:22 +0200

    made up a little header

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCard,MatCardContent],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
