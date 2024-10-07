788cc937a44efdc65caff5e9736ca2816681e3bc
Phillipcodes <Phillipsauer00@gmail.com>
2024-10-07 14:57:07 +0200

    ngoninit on the detail view to get the id out of the url

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,6 +10,17 @@ import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/m
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  userId:string|null = ''
+
+  constructor(private route:ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('userId');
+   
+    
+  }
 
 }
